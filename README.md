32cd4fa49428c9990973c5c20cafc4fbd3ccc44a
kirbyedy <kirbyedy@gmail.com>
2016-09-13 13:07:39 +0300

    changes 1st try

--- README.md
+++
@@ -1,2 +0,0 @@
-# module1-solution
-module 1 solution - angular on coursera 
--- app.js
+++ app.js
@@ -3,7 +3,24 @@
 
   angular.module('LunchCheck',[])
 
-  .controller(LunchCheckController, function(){
+  .controller('LunchCheckController', LunchCheckController);
 
-  });
+LunchCheckController.$inject = ['$scope'];
+
+  function LunchCheckController($scope){
+      $scope.food = "";
+      $scope.message = "";
+      $scope.checkFood = function() {
+        var foodArray = $scope.food.split(",");
+        if (foodArray == "") {
+          $scope.message = "Please enter data first!";
+        }
+        else if (foodArray.length <= 3) {
+          $scope.message = "Enjoy!";
+        }
+        else {
+          $scope.message = "Too much!";
+        }
+      };
+  };
 })();
